d76f6521c0c3ada5fde8d9e2175cff98b08ebfdc
Kapil <kapil.chaulagai@isteer.com>
2023-01-31 14:18:14 +0530

    Commit on 31-01-23

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -197,7 +197,7 @@ for (let [key, value] of events) {
   // ? console.log(`[FIRST HALF] ${key} : ${value}`)
   // : console.log(`[SECOND HALF] ${key} : ${value}`);
 } */
-
+/* 
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 
@@ -211,3 +211,26 @@ document.querySelector('button').addEventListener('click', function () {
     console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
   }
 });
+ */
+///////////////////////////////////////
+// String Methods Practice
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const flightsManage = flights.split('+');
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flightsManage) {
+  const [type, from, to, time] = flight.split(';');
+  let output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(':', 'h')})`;
+  console.log(output.padStart(50));
+}
